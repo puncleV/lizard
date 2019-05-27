a4baa82f88196d5b5aa863734c12269d0c3d07ad
punkkk <punkrus18@gmail.com>
2019-05-27 20:47:00 +0300

    initial commit

---
+++ Assets/Narrator.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Narrator
+{
+    private string _name;
+    private NumberPicker _numberPicker;
+
+    public Narrator(string name, NumberPicker numberPicker)
+    {
+        _name = name;
+        _numberPicker = numberPicker;
+    }
+
+    public void greet ()
+    {
+        Debug.Log(string.Concat("Welcome to Number Wizard Inc. ", this._name));
+    }
+
+    public void currentState ()
+    {
+        Debug.Log(string.Concat("Minimum:", _numberPicker.getMin()));
+        Debug.Log(string.Concat("Maximum:", _numberPicker.getMax()));
+    }
+
+    public void suggest ()
+    {
+        Debug.Log(string.Concat("Is it right:", _numberPicker.suggest(), "?"));
+    }
+
+    public void correct()
+    {
+        Debug.Log("EZ PZ LEMON SQUEEZE!");
+    }
+
+    public void lower()
+    {
+        this.beforeChooseMessage();
+        _numberPicker.lower();
+    }
+    public void highter()
+    {
+        this.beforeChooseMessage();
+        _numberPicker.highter();
+    }
+    private void beforeChooseMessage()
+    {
+        Debug.Log("Hm, let me think a bit more...");
+    }
+}
