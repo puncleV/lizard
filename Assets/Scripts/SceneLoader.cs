b6a3f875a5d2220d5079738a2d3378ddd656076a
punkkk <punkrus18@gmail.com>
2019-06-03 11:31:23 +0300

    add SceneLoader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    int currentScene = 0;
+
+    public void loadNextScene()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentScene + 1);
+
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+}
